2ffc3249bc0bc45cf69f12f6b37d77040aa71acb
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-10-14 21:39:27 -0600

    added titles to index file

--- setlist-creator-frontend/index.js
+++ setlist-creator-frontend/index.js
@@ -2,8 +2,14 @@ const header = document.querySelector('header');
 
 const allSongs = document.querySelector('.all-songs');
 allSongs.innerHTML =`<h3 lead text-center>Songs</h3>`;
-
 const addSongButton = document.createElement('button')
+
+const allSetlists = document.querySelector('#all-setlists')
+const allSetlistsTitle = document.querySelector('#all-setlists-title')
+allSetlistsTitle.innerHTML = `<section class='title-section'><h3 lead text-center>Set Lists <button id='add-setlist' class='btn btn-success'>+</button><button id='print-setlist' class='btn btn-secondary'><i class="fas fa-print"></i></button></h3><section>`
+const print = document.querySelector(`#print-setlist`)
+print.addEventListener('click', Setlist.printSet) 
+
 
 const songsAdapter = new SongsAdapter
 const setlistsAdapter = new SetlistsAdapter
